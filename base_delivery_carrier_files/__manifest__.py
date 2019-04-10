703e20a992a117c788d6515f507deff01560ffe0
Mariam Flayyan <mflayyan@therp.nl>
2019-04-11 02:40:11 +0300

    [MIG] base_delivery_carrier_files: Migration to 10.0

--- base_delivery_carrier_files/__manifest__.py
+++ base_delivery_carrier_files/__manifest__.py
@@ -4,26 +4,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Base Delivery Carrier Files',
-    'version': '8.0.1.2.4',
+    'version': '10.0.1.0.1',
     'category': 'Generic Modules/Warehouse',
+    'summary': """
+Base module for creation of carrier files (La Poste, TNT Express Shipper, ...).
+Files are exported as text (csv, ...).
+""",
     'author': "Camptocamp,Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'website': 'http://www.camptocamp.com',
-    'depends': [
-        'base',
-        'stock',
-        'delivery'
-    ],
-    'data': [
-        'views/carrier_file_view.xml',
-        'views/stock_view.xml',
-        'wizards/generate_carrier_files_view.xml',
-        'security/ir.model.access.csv'
-    ],
-    'demo': [
-        'demo/carrier_file_demo.xml',
-        'demo/carrier_file_demo.yml'
-    ],
+    'depends': ['base',
+                'stock',
+                'delivery'],
+    'data': ['carrier_file_view.xml',
+             'stock_view.xml',
+             'wizard/generate_carrier_files_view.xml',
+             'security/ir.model.access.csv'],
+    'demo': ['carrier_file_demo.xml', 'carrier_file_demo.yml'],
+    'test': [ '../account/test/account_minimal_test.xml'],
     'images': [],
     'installable': True,
     'auto_install': False,
